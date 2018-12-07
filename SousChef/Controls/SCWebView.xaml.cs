46b47e000095ca8d116a30c29e61bb29df3e2dba
markb <markb@work>
2018-12-07 08:04:01 +0100

    Improved border logic

--- SousChef/Controls/SCWebView.xaml.cs
+++ SousChef/Controls/SCWebView.xaml.cs
@@ -24,7 +24,8 @@ namespace SousChef.Controls
 
         public Guid webViewId { get; set; }
 
-        public string borderWidth { get => "5px"; }
+        public string borderWidth { get => borderWidthInt + "px"; }
+        public int borderWidthInt { get => 5; }
 
         private string currentUrl;
         private bool closeButtonVisible;
@@ -37,31 +38,95 @@ namespace SousChef.Controls
             this.notifyOfNavigation = notifyOfNavigation;
             this.notifyOfClosing = closePaneWithGuid;
 
-            borderLeft.PointerEntered += FlipCloseBluttonVisibility;
-            borderTop.PointerEntered += FlipCloseBluttonVisibility;
-            borderRight.PointerEntered += FlipCloseBluttonVisibility;
-            borderBottom.PointerEntered += FlipCloseBluttonVisibility;
-
-            borderTop.DragOver += FlipCloseBluttonVisibility;
+            borderLeft.PointerEntered += LeftBorderEntered;
+            borderTop.PointerEntered += TopBorderEntered;
+            borderRight.PointerEntered += RightBorderEntered;
+            borderBottom.PointerEntered += BottomBorderEntered;
+                        
+            //borderTop.DragEnter += TopBorderEntered;
 
             closeButton.Click += ClosePane;
 
             SetUpNavigationCheckTimer();
+            SetUpCloseCloseButtonBarTimer();
             Navigate(initUrl);
+        }        
+
+        #region Border Events
+
+        private void LeftBorderEntered(object sender, PointerRoutedEventArgs e)
+        {
+            var p = e.GetCurrentPoint(sender as UIElement);
+            //Coming in from the left
+            if (p.Position.X < (borderWidthInt - 1) * .5)
+                ShowCloseButtonBar();
+            else
+                HideCloseButtonBar();
         }
+
+        private void TopBorderEntered(object sender, PointerRoutedEventArgs e)
+        {
+            var p = e.GetCurrentPoint(sender as UIElement);
+            //Coming in from the left
+            if (p.Position.Y < (borderWidthInt - 1) * .5)
+                ShowCloseButtonBar();
+            else
+                HideCloseButtonBar();
+        }
+
+        private void RightBorderEntered(object sender, PointerRoutedEventArgs e)
+        {
+            var p = e.GetCurrentPoint(sender as UIElement);
+            //Coming in from the left
+            if (p.Position.X < (borderWidthInt - 1) * .5)
+                HideCloseButtonBar();
+            else
+                ShowCloseButtonBar();
+        }
+
+        private void BottomBorderEntered(object sender, PointerRoutedEventArgs e)
+        {
+            var p = e.GetCurrentPoint(sender as UIElement);
+            //Coming in from the left
+            if (p.Position.Y < (borderWidthInt - 1) * .5)
+                HideCloseButtonBar();
+            else
+                ShowCloseButtonBar();
+        }
+
+        #endregion
+
+        #region Close button bar
+
+        DispatcherTimer closeCloseButtonBarTimer;
+
+        private void SetUpCloseCloseButtonBarTimer()
+        {
+            closeCloseButtonBarTimer = new DispatcherTimer();
+            closeCloseButtonBarTimer.Tick += HideCloseButtonBar;
+            closeCloseButtonBarTimer.Interval = new TimeSpan(0, 0, 3);
+        }
+
+        private void HideCloseButtonBar(object sender = null, object e = null)
+        {
+            if (closeCloseButtonBarTimer.IsEnabled) closeCloseButtonBarTimer.Stop();
+            closeButtonBar.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowCloseButtonBar()
+        {
+            if (closeCloseButtonBarTimer.IsEnabled) closeCloseButtonBarTimer.Stop();
+            closeButtonBar.Visibility = Visibility.Visible;
+            closeCloseButtonBarTimer.Start();            
+        }
+
 
         private void ClosePane(object sender, RoutedEventArgs e)
         {
             notifyOfClosing(this.webViewId);
         }
 
-        private void FlipCloseBluttonVisibility(object sender, RoutedEventArgs e)
-        {
-            closeButtonVisible = !closeButtonVisible;
-            closeButtonBar.Visibility = closeButtonVisible ? Visibility.Visible : Visibility.Collapsed;
-
-            //Add timer to automatically hide the bar if it is currently showing after 3 seconds
-        }
+        #endregion
 
         private void SetUpNavigationCheckTimer()
         {
