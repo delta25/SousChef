29075b5121b6c0193da512f4fc58bb4e8afe49b3
markb <markb@work>
2018-12-10 08:52:14 +0100

    Playing around

--- SousChef/Controls/TestingControl.cs
+++ SousChef/Controls/TestingControl.cs
@@ -7,18 +7,33 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
 
 // The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
 
 namespace SousChef.Controls
 {
+    [ContentProperty(Name = "Content")]
     public sealed class TestingControl : Control
     {
         public TestingControl()
         {
             this.DefaultStyleKey = typeof(TestingControl);
+            
+            
+
+            var x  = (double)Resources["myHeight"];
+
         }
 
+        public object Content
+        {
+            get { return (string)GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register("Content", typeof(string), typeof(TestingControl), new PropertyMetadata(string.Empty));
     }
 }
