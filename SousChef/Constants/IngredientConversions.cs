a47bd2a611e50a366283ab737d57c18d6ea02e0f
markb <markb@work>
2018-12-20 15:22:22 +0100

    Working on converters

---
+++ SousChef/Constants/IngredientConversions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SousChef.Constants
+{
+    public class IngredientConversions
+    {
+        public class Names
+        {
+            public const string FahrenheitTocelsius = "Fahrenheit & Celsius";
+            public const string Butter = "Butter";
+            public const string Liquid = "Liquid";
+            public const string APFlour = "AP Flour";
+            public const string BreadFlour = "Bread Flour";
+            public const string RolledOats = "Rolled Oats";
+            public const string WhiteSugar = "Granulated Sugar";
+            public const string PackedBrownSugar = "Packed brown sugar";
+            public const string Honey = "Honey";
+        }
+
+        public class Formulas
+        {
+            public static class LeftToRight
+            {
+                public static readonly Func<double, double> FahrenheitToCelcius = (double fahrenheit) => (fahrenheit - 32) * 5/9;
+                public static readonly Func<double, double> ButterStickToGrams = (double butterSticks) => butterSticks * 113;
+                public static readonly Func<double, double> CupToMl = (double cup) => cup * 237;
+                public static readonly Func<double, double> APFlourCupsToGrams = (double cup) => cup * 128;
+                public static readonly Func<double, double> BreadFlourCupsToGrams = (double cup) => cup * 136;
+                public static readonly Func<double, double> OatsCupsToGrams = (double cup) => cup * 85;
+                public static readonly Func<double, double> WhiteSugarCupsToGrams = (double cup) => cup * 201;
+                public static readonly Func<double, double> PackedBrownSugarCupsToGrams = (double cup) => cup * 220;
+                public static readonly Func<double, double> HoneyCupsToGrams = (double cup) => cup * 340;
+            }
+
+            public class RightToLeft
+            {
+                public static readonly Func<double, double> CelciusToFahrenheit = (double celscius) => (celscius * 9/5) + 32;
+                public static readonly Func<double, double> GramsToButterStick = (double grams) => grams / 113;
+                public static readonly Func<double, double> MlToCup= (double ml) => ml / 237;
+                public static readonly Func<double, double> GramsToAPFlourCups = (double grams) => grams / 128;
+                public static readonly Func<double, double> GramsToBreadFlourCups = (double grams) => grams / 136;
+                public static readonly Func<double, double> GramsToOatsCups = (double grams) => grams / 85;
+                public static readonly Func<double, double> GramsToWhiteSugarCups = (double grams) => grams / 201;
+                public static readonly Func<double, double> GramsToPackedBrownSugarCups = (double grams) => grams / 220;
+                public static readonly Func<double, double> GramsToHoneyCups = (double grams) => grams / 340;
+            }
+
+        }
+    }
+}
---
+++ SousChef/Pages/ConvertersPage.xaml.cs
@@ -0,0 +1,150 @@
+﻿using Newtonsoft.Json;
+using SousChef.Constants;
+using SousChef.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SousChef.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ConvertersPage : Page
+    {
+
+        public UnitConversion selectedConverter;
+
+        public ConvertersPage()
+        {
+            this.InitializeComponent();
+
+            //PopulateConverterOptions();
+            LoadConvertersAsync();
+        }
+
+        private async Task LoadConvertersAsync()
+        {
+            
+
+                //var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Resources/Converters.csv"));
+
+        }
+
+        private void PopulateConverterOptions()
+        {
+            var fahrenheitTocelsius = new IngredientConversion()
+            {
+                Name = IngredientConversions.Names.FahrenheitTocelsius,
+                FormulaLeftToRight = IngredientConversions.Formulas.LeftToRight.FahrenheitToCelcius,
+                FormulaRightToLeft = IngredientConversions.Formulas.RightToLeft.CelciusToFahrenheit
+            };
+
+            var butter = new IngredientConversion()
+            {
+                Name = IngredientConversions.Names.Butter,
+                FormulaLeftToRight = IngredientConversions.Formulas.LeftToRight.ButterStickToGrams,
+                FormulaRightToLeft = IngredientConversions.Formulas.RightToLeft.GramsToButterStick
+            };
+
+            var liquid = new IngredientConversion()
+            {
+                Name = IngredientConversions.Names.Liquid,
+                FormulaLeftToRight = IngredientConversions.Formulas.LeftToRight.CupToMl,
+                FormulaRightToLeft = IngredientConversions.Formulas.RightToLeft.MlToCup
+            };
+
+            var aPFlour = new IngredientConversion()
+            {
+                Name = IngredientConversions.Names.APFlour,
+                FormulaLeftToRight = IngredientConversions.Formulas.LeftToRight.APFlourCupsToGrams,
+                FormulaRightToLeft = IngredientConversions.Formulas.RightToLeft.GramsToAPFlourCups
+            };
+
+            var breadFlour = new IngredientConversion()
+            {
+                Name = IngredientConversions.Names.BreadFlour,
+                FormulaLeftToRight = IngredientConversions.Formulas.LeftToRight.BreadFlourCupsToGrams,
+                FormulaRightToLeft = IngredientConversions.Formulas.RightToLeft.GramsToBreadFlourCups
+            };
+
+            var rolledOats = new IngredientConversion()
+            {
+                Name = IngredientConversions.Names.RolledOats,
+                FormulaLeftToRight = IngredientConversions.Formulas.LeftToRight.OatsCupsToGrams,
+                FormulaRightToLeft = IngredientConversions.Formulas.RightToLeft.GramsToOatsCups
+            };
+
+            var whiteSugar = new IngredientConversion()
+            {
+                Name = IngredientConversions.Names.WhiteSugar,
+                FormulaLeftToRight = IngredientConversions.Formulas.LeftToRight.WhiteSugarCupsToGrams,
+                FormulaRightToLeft = IngredientConversions.Formulas.RightToLeft.GramsToWhiteSugarCups
+            };
+
+            var packedBrownSugar = new IngredientConversion()
+            {
+                Name = IngredientConversions.Names.PackedBrownSugar,
+                FormulaLeftToRight = IngredientConversions.Formulas.LeftToRight.PackedBrownSugarCupsToGrams,
+                FormulaRightToLeft = IngredientConversions.Formulas.RightToLeft.GramsToPackedBrownSugarCups
+            };
+
+            var honey = new IngredientConversion()
+            {
+                Name = IngredientConversions.Names.Honey,
+                FormulaLeftToRight = IngredientConversions.Formulas.LeftToRight.HoneyCupsToGrams,
+                FormulaRightToLeft = IngredientConversions.Formulas.RightToLeft.GramsToHoneyCups
+            };
+
+            var unitConversionList = new List<UnitConversion>
+            {
+                new UnitConversion()
+                {
+                    Name = UnitConversions.Names.Temperature,
+                    LeftImage = UnitConversions.Icons.Fahrenheit,
+                    RightImage = UnitConversions.Icons.Celsius,
+                    IngredientConversions = new List<IngredientConversion>() { fahrenheitTocelsius }
+                },
+                new UnitConversion()
+                {
+                    Name = UnitConversions.Names.ButterAndGrams,
+                    LeftImage = UnitConversions.Icons.ButterStick,
+                    RightImage = UnitConversions.Icons.Weight,
+                    IngredientConversions = new List<IngredientConversion>() { butter }
+                },
+                new UnitConversion()
+                {
+                    Name = UnitConversions.Names.CupsAndML,
+                    LeftImage = UnitConversions.Icons.ButterStick,
+                    RightImage = UnitConversions.Icons.Weight,
+                    IngredientConversions = new List<IngredientConversion>() { liquid }
+                },
+                new UnitConversion()
+                {
+                    Name = UnitConversions.Names.CupsAndGrams,
+                    LeftImage = UnitConversions.Icons.ButterStick,
+                    RightImage = UnitConversions.Icons.Weight,
+                    IngredientConversions = new List<IngredientConversion>() { aPFlour, breadFlour, rolledOats, whiteSugar, packedBrownSugar, honey }
+                }
+            };
+
+            converterListing.ItemsSource = unitConversionList;
+        }
+    }
+
+}
